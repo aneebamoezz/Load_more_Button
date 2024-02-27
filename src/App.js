b4a4fdf9cc7f9e2637a15d01c919f037e3e79950
aneebamoezz <anibakaleem@gmail.com>
2024-02-27 14:48:16 +0500

    commit initial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoadMoreButton from './components/LoadMoreButton';
+
+function App() {
+  return (
+    <div className="App">
+      <LoadMoreButton />
+    </div>
+  );
+}
+
+export default App;
